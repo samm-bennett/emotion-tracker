bcbb9509a43c0d7d9320c8c7185e85908ccd4b25
Thomas Bennett <thomas.j.bennett@gmail.com>
2014-07-07 14:11:30 -0700

    Initial Commit

---
+++ Emotion Tracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Emotion Tracker
+//
+//  Created by Thomas Bennett on 7/7/14.
+//  Copyright (c) 2014 Thomas Bennett. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
